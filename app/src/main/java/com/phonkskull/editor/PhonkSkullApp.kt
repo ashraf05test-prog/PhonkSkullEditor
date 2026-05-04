7804365e5464472596518be89b4e8184bb07fbd8
ashraf05test-prog <ashraf05.test@gmail.com>
2026-05-05 03:49:32 +0600

    Fix startup crash

--- app/src/main/java/com/phonkskull/editor/PhonkSkullApp.kt
+++ app/src/main/java/com/phonkskull/editor/PhonkSkullApp.kt
@@ -1,11 +1,9 @@
 package com.phonkskull.editor
 
 import android.app.Application
-import com.arthenica.ffmpegkit.FFmpegKitConfig
 
 class PhonkSkullApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        FFmpegKitConfig.enableLogCallback(null)
     }
 }
